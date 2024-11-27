4d0605f83057a34cedd665f0653af0cba85bdeb6
Autumn (Arch) <arch1010@pm.me>
2024-11-27 17:03:37 -0600

    Formatting :3 + Readme changed

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Silly Cursors
  
-https://cloud-blw6hm7ot-hack-club-bot.vercel.app/0image.png
+![image](https://cloud-blw6hm7ot-hack-club-bot.vercel.app/0image.png)
+
+Yet another silly little website that's in the /docs folder. 
+
+Try it [here](https://cursors.arch1010.dev)!
--- docs/getcursor.js
+++ docs/getcursor.js
@@ -1,5 +1,22 @@
-let cursor_image = sessionStorage.getItem("image");
+window.onload = function() {
+    const canvas = document.getElementById('drawingCanvas');
+    const exportButton = document.getElementById('exportButton');
+
+    let cursor_image = sessionStorage.getItem("image");
+
+    // Function to export the cursor image
+    function exportCursor() {
+        const link = document.createElement('a');
+        link.href = cursor_image;
+        link.download = 'cursor.png';
+        link.click();
+    }
+
+    // Add click event listener to the export button
+    exportButton.addEventListener('click', exportCursor);
 
 
-// Set the data URL as the cursor for the entire body
-document.body.style.cursor = 'url(' + cursor_image + ') 12 12, auto';
+    // Set the data URL as the cursor for the entire body
+    document.body.style.cursor = 'url(' + cursor_image + ') 12 12, auto';
+
+};
